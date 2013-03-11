b3d9521673e34f4091bd444ac51e2773dfdd16c7
vaniaspeedy <vaniaspeedy@gmail.com>
2013-03-10 23:00:57 -0700

    added test script

---
+++ cron.sh
@@ -0,0 +1,5 @@
+cp -v website_bak.sh /etc/cron.daily/
+
+# keep separate scripts for all rotations - based on cron
+0 */4 * * * /usr/local/bin/make_snapshot.sh
+0 13 * * *  /usr/local/bin/daily_snapshot_rotate.sh
